646aea8fd1713ed0a4bf35488e5b2972acb18c5b
Shawn Rutledge <shawn.rutledge@theqtcompany.com>
2017-02-06 22:02:03 +0100

    read data, insert into InfluxDB and exit

--- influx-inserter.go
+++ influx-inserter.go
@@ -1,9 +1,14 @@
 package main
 
-import "encoding/json"
-import "fmt"
-import "io/ioutil"
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+	"log"
+	influx "github.com/influxdata/influxdb/client/v2"
+)
 
 // {"data":{ "id":"41000008","type":"4","detector":"SBM20","voltage":379,"cpm":31,"temperature":11.00,"pressure":99815,"uptime": 480}}
 type URadMonitorData struct {
@@ -34,11 +39,43 @@ func main() {
 		return
 	}
 	fmt.Println(body)
-	var data URadMonitorDataData
-	err = json.Unmarshal(body, &data)
+	var dataData URadMonitorDataData
+	err = json.Unmarshal(body, &dataData)
+	data := dataData.Data
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(data.Data)
+	fmt.Println(data)
+	influxUrl := "http://localhost:8086"
+	client, err := influx.NewHTTPClient(influx.HTTPConfig{Addr: influxUrl})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bp, _ := influx.NewBatchPoints(influx.BatchPointsConfig{
+		Database:  "weather",
+	})
+	point, err := influx.NewPoint(
+		"uradmonitor",
+		map[string]string{
+			"stationId": data.Id,
+		},
+		map[string]interface{}{
+			"voltage":  data.Voltage,
+			"cpm": data.Cpm,
+			"temperature": data.Temperature,
+			"pressure": data.Pressure,
+		},
+		time.Now(),
+	)
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	bp.AddPoint(point)
+	err = client.Write(bp)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
